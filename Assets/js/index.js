8310f028259aa9450ea384748873aa25827364a0
afcardenas662 <anafelipe.567@gmail.com>
2021-11-15 21:26:33 -0500

    segundaparte

---
+++ Assets/js/index.js
@@ -0,0 +1,4 @@
+var getdata= function (){
+    var nombre_estudiante = document.getElementById("nombre_estudiante").value;
+    var apellido_estudiante = document.getElementById("apellido_estudiante").value;
+}
